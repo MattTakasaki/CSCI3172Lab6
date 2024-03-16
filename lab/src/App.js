dba72b0ef241b73dee19a58bd6b242260e40a4cd
matt <mattakasaki@gmail.com>
2024-03-16 16:47:05 -0300

    new commit

---
+++ lab/src/App.js
@@ -0,0 +1,64 @@
+
+import './App.css';
+
+function App() {
+  return (
+    
+    <html lang = 'en'>
+      <head>
+        <title>lab6</title>
+      </head>
+    <body>
+    <div className="lab6">
+      <header className="header">
+        
+        <p> Welcome to my Lab 6 </p>
+        
+      </header>
+
+      <div className="title-section">
+
+        <h2 className="main-Title">Lab 6</h2>
+        
+
+      </div>
+      <div className="about-section">
+        <section className="about-portion">
+          <h3>Lorem Ipsum</h3>
+          <p>
+          Aliquam porta sapien eget sagittis aliquet. Integer quis blandit elit, ac cursus sapien. Interdum et malesuada fames ac ante ipsum primis in faucibus. Nunc ligula odio, viverra nec justo vel, dignissim luctus nisi. Aliquam sed nunc sit amet erat faucibus fringilla non non erat. Sed volutpat malesuada nibh, a egestas lectus. Nunc euismod iaculis dictum. Suspendisse eleifend, tortor quis mattis dictum, mi mauris dictum erat, ac faucibus lorem lectus quis orci.
+          </p>
+          <img src = {require("./stock.jpg")} alt = "stock code" ></img>
+        </section>
+        <section className="about-portion">
+          <h3>Lorem Ipsum</h3>
+          <p>
+          Aliquam porta sapien eget sagittis aliquet. Integer quis blandit elit, ac cursus sapien. Interdum et malesuada fames ac ante ipsum primis in faucibus. Nunc ligula odio, viverra nec justo vel, dignissim luctus nisi. Aliquam sed nunc sit amet erat faucibus fringilla non non erat. Sed volutpat malesuada nibh, a egestas lectus. Nunc euismod iaculis dictum. Suspendisse eleifend, tortor quis mattis dictum, mi mauris dictum erat, ac faucibus lorem lectus quis orci.
+          </p>
+          <img src = {require("./stock.jpg")} alt = "stock code" ></img>
+        </section>
+        <section className="about-portion">
+          
+          <h3>Lorem Ipsum</h3>
+          <p>
+          Aliquam porta sapien eget sagittis aliquet. Integer quis blandit elit, ac cursus sapien. Interdum et malesuada fames ac ante ipsum primis in faucibus. Nunc ligula odio, viverra nec justo vel, dignissim luctus nisi. Aliquam sed nunc sit amet erat faucibus fringilla non non erat. Sed volutpat malesuada nibh, a egestas lectus. Nunc euismod iaculis dictum. Suspendisse eleifend, tortor quis mattis dictum, mi mauris dictum erat, ac faucibus lorem lectus quis orci.
+          </p>
+          <img src = {require("./stock.jpg")} alt = "stock code" ></img>
+        </section>
+        <section className="about-portion">
+          <h3>Lorem Ipsum</h3>
+          <p>
+          Aliquam porta sapien eget sagittis aliquet. Integer quis blandit elit, ac cursus sapien. Interdum et malesuada fames ac ante ipsum primis in faucibus. Nunc ligula odio, viverra nec justo vel, dignissim luctus nisi. Aliquam sed nunc sit amet erat faucibus fringilla non non erat. Sed volutpat malesuada nibh, a egestas lectus. Nunc euismod iaculis dictum. Suspendisse eleifend, tortor quis mattis dictum, mi mauris dictum erat, ac faucibus lorem lectus quis orci.
+          </p>
+          <img src = {require("./stock.jpg")} alt = "stock code" ></img>
+        </section>      
+      </div>
+
+    </div>
+    </body>
+    </html>
+
+  );
+}
+
+export default App;
---
+++ lab/src/README.txt
@@ -0,0 +1,8 @@
+
+author: Matthew Takasaki, B00901845
+
+
+references: 
+
+    Text content from https://www.lipsum.com/ 
+    stock.jpg image from https://www.istockphoto.com/photo/programming-code-abstract-technology-background-of-software-deve-gm537331500-95204155 
